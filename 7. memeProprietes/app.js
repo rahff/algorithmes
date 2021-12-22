1f9801b94d4e533c94fa2d1ecbca5281688eb3be
rahff <raphaelandrey99@gmail.com>
2021-12-22 14:21:12 +0100

    algo n 7 done

--- 7. memeProprietes/app.js
+++ 7. memeProprietes/app.js
@@ -1,13 +1,29 @@
-function memeProps(obj, source){
-
-
+function memeProps(obj, test){
+    let result = false;
+    const keysOfObj = Object.keys(obj)
+    const keysOfTest = Object.keys(test)
+    for (let i = 0; i < keysOfObj.length; i++) {
+      if(keysOfTest.includes(keysOfObj[i])){
+          if(obj[keysOfObj[i]] === test[keysOfObj[i]]){
+            result = true
+          }else{
+              result = false;
+              break;
+          }
+      }else{
+          result = false;
+      }
+    }
+    return result;
 }
 
 
 console.log(memeProps({age: 44, taille: 188}, { age: 44, taille: 188 })); // true 
 console.log(memeProps({age: 44, taille: 188}, { age: 24, taille: 180 })); // false 
 console.log(memeProps({age: 44, taille: 188}, { age: 10, taille: 148 })); // false 
-
+console.log(memeProps({a:1,b: 5}, {a:2, b:5})); //false
+console.log(memeProps({p:"ere", i:5}, {m:"ere", i:"5"})); // false
+console.log(memeProps({l:"g", v:"s"}, {l:"g", v:"s"}))//true
 
 
 // -----------------  Énoncé ------------------- //
